376c60045eadb8e0db8ae0e745829f1f6f41d18d
Henry Buckle <henry.buckle@hiscox.com>
2018-03-01 10:30:34 +0000

    add puppet role param

--- windows/puppet_agent.ps1
+++ windows/puppet_agent.ps1
@@ -1,12 +1,12 @@
 [CmdletBinding()]
 param(
-  [string]$proxy,
   [string]$dns_suffix,
   [Parameter(Mandatory = $true)]
   [string]$puppet_master,
   [string]$puppet_environment = "production",
   [string]$puppet_autosign_key,
-  [string]$puppet_agent_url = "https://downloads.puppetlabs.com/windows/puppet5/puppet-agent-x64-latest.msi"
+  [string]$puppet_agent_url = "https://downloads.puppetlabs.com/windows/puppet5/puppet-agent-x64-latest.msi",
+  [string]$puppet_role
 )
 $ProgressPreference = "SilentlyContinue"
 $ErrorActionPreference = "Stop"
@@ -14,9 +14,6 @@ $ErrorActionPreference = "Stop"
 & netsh.exe Advfirewall set allprofiles state off
 
 $client = New-Object System.Net.WebClient
-if ($proxy) {
-  $client.Proxy = New-Object System.Net.WebProxy($proxy)
-}
 $client.DownloadFile($puppet_agent_url, "C:\puppet-agent.msi")
 $certName = $env:COMPUTERNAME + ".${dns_suffix}"
 $certName = $certName.ToLower() #Puppet cert names must be lower case
@@ -26,9 +23,9 @@ Start-Process "msiexec.exe" -ArgumentList $installargs -Wait
 $csrFile = 'C:\ProgramData\PuppetLabs\puppet\etc\csr_attributes.yaml'
 $content = @"
 custom_attributes:
-  challengePassword: '${puppet_autosign_key}'
+  challengePassword: '$puppet_autosign_key'
 extension_requests:
-  pp_role: '${puppet_role}'
+  pp_role: '$puppet_role'
 "@
 Set-Content $csrFile $content -Encoding UTF8
 
